3bf8c162f194fd91b67110f85e84a10e23c1b9ce
ranjithsvcetcse <59575714+ranjithsvcetcse@users.noreply.github.com>
2020-01-09 19:23:41 +0530

    Addition program in c
    This is a basic addition program in C language.

---
+++ addition.c
@@ -0,0 +1,17 @@
+#include<studio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+printf("Enter a value : ");
+scanf("%d",&a);
+printf("Enter b value : ");
+scanf("%d",&b);
+printf("Sum of %d and %d is %d.",a,b,a+b);
+getch();
+}
+
+SAMPLE OUTPUT:
+Enter a value : 5
+Enter b value : 10
+Sum of 5 and 10 is 15.
