6a6d0443bea996b465e6a055438e165d89015cf2
ranjithsvcetcse <59575714+ranjithsvcetcse@users.noreply.github.com>
2020-01-09 19:28:49 +0530

    Subtraction program
    This is a basic C program for Subtraction.

---
+++ subtraction.c
@@ -0,0 +1,17 @@
+#include<studio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+printf("Enter a value : ");
+scanf("%d",&a);
+printf("Enter b value : ");
+scanf("%d",&b);
+printf("Subtraction of %d from %d is %d.",b,a,a-b);
+getch();
+}
+
+SAMPLE OUTPUT:
+Enter a value : 10
+Enter b value : 5
+Subtraction of 5 from 10 is 5.
